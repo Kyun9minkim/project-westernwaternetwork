ca2fc78272f9eb2e3c870dc77b7ccb5767fbf5d5
kyun9minkim <kyun9minkim@gmail.com>
2025-06-10 23:26:39 -0700

    add variables to model code

--- model_codes/Modelcode.R
+++ model_codes/Modelcode.R
@@ -2,3 +2,33 @@
 #author: Kyungmin Kim
 #date: June 11 2025
 
+
+#Simulation Period
+
+T <- 120 # 10 years (from 2010 to 2019)
+dt <- 1 # time step is one month
+
+#input data
+
+streamflow <- read.csv
+
+#for California
+
+DCA #demand
+QCA <- read.csv()
+
+#for Colorado
+
+DCO #demand
+QCO
+
+#for Rio Grande
+
+DRG <- #demand
+QRG
+
+#Transporting water
+
+QCOCA #water from CO to CA
+QCORG #water from CO to RG
+
